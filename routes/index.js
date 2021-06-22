ae223cfb6e5546ae94cd0210579a33dd3be19259
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-22 16:31:23 +0700

    Done assignment 4

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+  .route('/')
+  .get((req, res, next) => {
+    res.render('index', { title: 'Express' });
+  })
+  .post((req, res) => {
+    res
+      .status(403)
+      .end('POST operation not supported on ' + req.headers.host + '/');
+  })
+  .put((req, res) => {
+    res
+      .status(403)
+      .end('PUT operation not supported on ' + req.headers.host + '/');
+  })
+  .delete((req, res) => {
+    res
+      .status(403)
+      .end('DELETE operation not supported on ' + req.headers.host + '/');
+  });
 
 module.exports = router;
