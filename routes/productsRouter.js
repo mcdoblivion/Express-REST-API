abad8599fbbb17954c90a2536fd13a0d78b4aeef
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-23 01:56:48 +0700

    Comments API

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -45,6 +45,15 @@ productsRouter
     Products.findById(req.params.productId)
       .then(
         (product) => {
+          if (!product)
+            return res.status(404).json({
+              success: false,
+              msg:
+                'The product with id ' +
+                req.params.productId +
+                ' is not exist!',
+            });
+
           res.status(200).json(product);
         },
         (err) => next(err)
@@ -58,6 +67,13 @@ productsRouter
       { new: true }
     )
       .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
         res.status(200).json(product);
       })
       .catch((err) => next(err));
@@ -71,7 +87,151 @@ productsRouter
         (err) => next(err)
       )
       .catch((err) => next(err));
-    res.sendStatus(200);
+  });
+
+// /products/:productId/comments
+productsRouter
+  .route('/:productId/comments')
+  .get((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
+        res.status(200).json(product.comments);
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
+        product.comments.push(req.body);
+        product.save().then(
+          (product) => {
+            res.status(201).json(product);
+          },
+          (err) => next(err)
+        );
+      })
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
+        product.comments = [];
+        product
+          .save()
+          .then(
+            (product) => {
+              res.status(200).json(product);
+            },
+            (err) => next(err)
+          )
+          .catch((err) => next(err));
+      })
+      .catch((err) => next(err));
+  });
+
+// /products/:productId/comments/:commentId
+productsRouter
+  .route('/:productId/comments/:commentId')
+  .get((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
+        let comment = product.comments.id(req.params.commentId);
+        if (!comment)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The comment with id ' + req.params.commentId + ' is not exist!',
+          });
+
+        res.status(200).json(product.comments.id(req.params.commentId));
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
+        let comment = product.comments.id(req.params.commentId);
+        if (!comment)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The comment with id ' + req.params.commentId + ' is not exist!',
+          });
+
+        if (req.body.rating) comment.rating = req.body.rating;
+        if (req.body.comment) comment.comment = req.body.comment;
+
+        product.save().then(
+          (product) => {
+            res.status(200).json(product);
+          },
+          (err) => next(err)
+        );
+      })
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then((product) => {
+        if (!product)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The product with id ' + req.params.productId + ' is not exist!',
+          });
+
+        let comment = product.comments.id(req.params.commentId);
+        if (!comment)
+          return res.status(404).json({
+            success: false,
+            msg:
+              'The comment with id ' + req.params.commentId + ' is not exist!',
+          });
+
+        product.comments.id(req.params.commentId).remove();
+        product.save().then(
+          (product) => {
+            res.status(200).json(product);
+          },
+          (err) => next(err)
+        );
+      })
+      .catch((err) => next(err));
   });
 
 module.exports = productsRouter;
