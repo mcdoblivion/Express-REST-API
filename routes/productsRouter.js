6f245b93d11b73bbdd34c131fb4cb0fece9f7db3
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-23 00:05:06 +0700

    RESTful API, done assignment 5

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,24 +3,74 @@ const productsRouter = express.Router();
 
 const Products = require('../models/products');
 
+// /products/
 productsRouter
   .route('/')
   .get((req, res, next) => {
-    res.status(200).json({ success: true, data: {} });
+    Products.find({})
+      .then(
+        (products) => {
+          res.status(200).json({ success: true, data: products });
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
   .post((req, res, next) => {
-    res.status(200).json({ success: true, data: {} });
+    Products.create(req.body)
+      .then(
+        (product) => {
+          console.log('Product created:', product);
+          res.status(201).json(product);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   })
   .delete((req, res, next) => {
-    res.sendStatus(200);
+    Products.deleteMany({})
+      .then(
+        (response) => {
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   });
 
+// /products/:productId
 productsRouter
   .route('/:productId')
+  .get((req, res, next) => {
+    Products.findById(req.params.productId)
+      .then(
+        (product) => {
+          res.status(200).json(product);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
   .put((req, res, next) => {
-    res.sendStatus(200);
+    Products.findByIdAndUpdate(
+      req.params.productId,
+      { $set: req.body },
+      { new: true }
+    )
+      .then((product) => {
+        res.status(200).json(product);
+      })
+      .catch((err) => next(err));
   })
   .delete((req, res, next) => {
+    Products.findByIdAndDelete(req.params.productId)
+      .then(
+        (response) => {
+          res.status(200).json(response);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
     res.sendStatus(200);
   });
 
