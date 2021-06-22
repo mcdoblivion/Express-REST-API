35fe65dccb6fd2e5c76fb87d559886c585a46e57
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-22 17:17:38 +0700

    Install mongoose and create Product model

---
+++ models/products.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Currency = mongoose.Types.Currency;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Currency,
+      required: true,
+    },
+    comments: [commentSchema],
+  },
+  { timestamps: true }
+);
+
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Products = mongoose.model('Products', productSchema);
+
+module.exports = Products;
