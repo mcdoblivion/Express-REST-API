132df1c453e9951c7553f0233f61b421068c4eac
mcd_oblivion <mcd.oblivion@gmail.com>
2021-07-06 00:41:12 +0700

    Group all models to index.js

---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  CartItems: require('./cartItems'),
+  Comments: require('./comments'),
+  OrderItems: require('./orderItems'),
+  Orders: require('./orders'),
+  Products: require('./products'),
+  Users: require('./users'),
+};
