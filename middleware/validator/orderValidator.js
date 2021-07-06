4dde8b75d53b21418db29d447864f500687d940c
mcd_oblivion <mcd.oblivion@gmail.com>
2021-07-06 16:11:49 +0700

    Fix order validator

--- middleware/validator/orderValidator.js
+++ middleware/validator/orderValidator.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const actions = require('../../actions');
 
 const orderSchema = Joi.array().items(
   Joi.object({
@@ -8,12 +9,22 @@ const orderSchema = Joi.array().items(
   })
 );
 
-module.exports.validateOrder = (req, res, next) => {
+module.exports.validateOrder = async (req, res, next) => {
   const validateResult = orderSchema.validate([...req.body]);
   if (validateResult.error) {
     const err = new Error(validateResult.error);
     err.status = 400;
     next(err);
   }
+
+  for (const item of req.body) {
+    const product = await actions.productsActions.getProductById(item.product);
+    if (product.numberInStock < item.quantity) {
+      const err = new Error('Product quantity must less than number in stock!');
+      err.status = 400;
+      next(err);
+    }
+  }
+
   next();
 };
